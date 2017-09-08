0281907068f359ac7e8cf16e343fdb496f88abfa
Robert Smith <robertsmith@Roberts-MacBook-Pro.local>
2017-09-08 16:22:58 +0100

    WIP modal accessiblity

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -75,88 +75,28 @@ class App extends Component {
     const { isModalOpen } = this.state
     return (
       <div>
+        <Modal.Wrapper ariaLabel="my modal" closeModal={this.closeModal} isOpen={isModalOpen}>
+          <Modal.Content>
+            <Modal.Close>
+              <Modal.Dismiss>Close</Modal.Dismiss>
+            </Modal.Close>
+            <p>This is modal content</p>
+            <p>This is modal content</p>
+            <p>This is modal content</p>
+            <p>This is modal content</p>
+            <p>This is modal content</p>
+            <p>ITCSS and BEMIT based Sass/CSS framework that is ultra exensible
+              and scales to any project size.</p>
+            <Modal.Close>
+              <Button size="sm" theme="alpha">Close me</Button>
+            </Modal.Close>
+          </Modal.Content>
+        </Modal.Wrapper>
         <Section size="md">
           <SiteWrap padding>
             <h1>Nebula React</h1>
             <Button size="md" theme="alpha" onClick={this.openModal}>
               Open Modal
-            </Button>
-            <Modal.Wrapper closeModal={this.closeModal} isOpen={isModalOpen}>
-              <Modal.Content>
-                <Modal.Close>
-                  <Modal.Dismiss>Close</Modal.Dismiss>
-                </Modal.Close>
-                <p>This is modal content</p>
-                <p>This is modal content</p>
-                <p>This is modal content</p>
-                <p>This is modal content</p>
-                <p>This is modal content</p>
-                <p>ITCSS and BEMIT based Sass/CSS framework that is ultra exensible
-                  and scales to any project size.</p>
-                <Modal.Close>
-                  <Button size="sm" theme="alpha">Close me</Button>
-                </Modal.Close>
-              </Modal.Content>
-            </Modal.Wrapper>
-            <h2>Lists</h2>
-            <Form.Radio id="radio-1" value="one" name="radio-group" checked onChange={() => {}}>Option 1</Form.Radio>
-            <Form.Radio id="radio-2" value="two" name="radio-group">Option 2</Form.Radio>
-            <Form.Radio id="radio-3" value="two" name="radio-group" disabled>Option 3</Form.Radio>
-            <Form.Checkbox id="checkbox-1">Option 1</Form.Checkbox>
-            <Form.Checkbox id="checkbox-2" checked disabled>Option 2</Form.Checkbox>
-            <Form.Checkbox id="checkbox-3" disabled>Option 3</Form.Checkbox>
-            <Form.Select value="default" name="select-name" onChange={() => {}}>
-              <option value="default">Select option</option>
-              <option value="1">Option 1</option>
-              <option value="2" disabled>Option 2</option>
-              <option value="3">Option 3</option>
-            </Form.Select>
-            <Form.Search submitPosition="left" />
-            <Card>
-              <h3>Card Title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-             Ab adipisci architecto assumenda et expedita fugit id illum.</p>
-            </Card>
-            <StatusCard status="error">
-            Error
-          </StatusCard>
-            <Flyout.Wrapper>
-              <Flyout.Toggle>
-              Click me
-            </Flyout.Toggle>
-              <Flyout.Content direction="se">
-                <Card>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Ab adipisci architecto assumenda et expedita fugit id illum
-              </Card>
-              </Flyout.Content>
-            </Flyout.Wrapper>
-            <Flyout.Wrapper>
-              <Flyout.Toggle>
-                <Button node="span" size="sm" theme="alpha">
-                Github
-              </Button>
-              </Flyout.Toggle>
-              <Flyout.Content direction="ne">
-                <Card>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Ab adipisci architecto assumenda et expedita fugit id illum
-              </Card>
-              </Flyout.Content>
-            </Flyout.Wrapper>
-            <Flyout.Wrapper>
-              <Flyout.Toggle>
-              Click me
-            </Flyout.Toggle>
-              <Flyout.Content>
-                <Card>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Ab adipisci architecto assumenda et expedita fugit id illum
-              </Card>
-              </Flyout.Content>
-            </Flyout.Wrapper>
-            <Button type="submit" size="md" theme="beta">
-              Button test
             </Button>
             <h3>Button Dropdowns</h3>
             <ButtonDropdown.Wrapper togglePosition="left">
--- src/components/Modal/Wrapper.jsx
+++ src/components/Modal/Wrapper.jsx
@@ -1,5 +1,6 @@
 import { Component, createElement as E } from 'react'
 import T from 'prop-types'
+import noScroll from 'no-scroll'
 
 import { classNames } from '../../utils/'
 
@@ -8,25 +9,61 @@ class ModalWrapper extends Component {
     closeModal: this.props.closeModal
   })
 
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp)
+  }
+
+  componentDidUpdate(prevProps) {
+    if ((prevProps.isOpen !== this.props.isOpen)) {
+      if (this.props.isOpen) {
+        noScroll.on()
+        setTimeout(() => {
+          this.bodyNode.focus()
+        }, 0)
+      } else {
+        noScroll.off()
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp)
+  }
+
+  handleKeyUp = (e) => {
+    if (e.keyCode === 27) {
+      this.props.closeModal()
+    }
+  }
+
   render() {
-    const { node, closeModal, className, isOpen, children, ...rest } = this.props
+    const { node, closeModal, className, isOpen, ariaLabel, children, ...rest } = this.props
 
     return E(
       node || 'div',
       {
         className: classNames('c-modal', { 'is-open': isOpen }, className),
+        role: 'dialog',
+        'aria-label': ariaLabel,
+        'aria-hidden': !isOpen,
         ...rest
       },
       E(
         'button',
         {
           className: 'c-modal__overlay',
-          onClick: closeModal
+          'aria-hidden': true,
+          onClick: closeModal,
+          tabIndex: -1
         }
       ),
       E(
         'div',
-        { className: 'c-modal__body' },
+        {
+          className: 'c-modal__body',
+          ref: (n) => { this.bodyNode = n },
+          tabIndex: isOpen ? 1 : -1
+        },
         children
       )
     )
@@ -41,8 +78,9 @@ ModalWrapper.propTypes = {
   node: T.string,
   className: T.string,
   isOpen: T.bool.isRequired,
+  ariaLabel: T.string.isRequired,
   closeModal: T.func.isRequired,
-  children: T.node.isRequired
+  children: T.element.isRequired
 }
 
 export default ModalWrapper
