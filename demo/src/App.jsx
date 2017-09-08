03672c4a0848f3dc70811d3205f27e29b1cf3407
Robert Smith <rbrtsmith84@googlemail.com>
2017-09-08 19:43:46 +0100

    fix demo issues

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -98,66 +98,7 @@ class App extends Component {
                 </Modal.Close>
               </Modal.Content>
             </Modal.Wrapper>
-            <h2>Lists</h2>
-            <Form.Radio id="radio-1" value="one" name="radio-group" checked onChange={() => {}}>Option 1</Form.Radio>
-            <Form.Radio id="radio-2" value="two" name="radio-group">Option 2</Form.Radio>
-            <Form.Radio id="radio-3" value="two" name="radio-group" disabled>Option 3</Form.Radio>
-            <Form.Checkbox id="checkbox-1">Option 1</Form.Checkbox>
-            <Form.Checkbox id="checkbox-2" checked disabled>Option 2</Form.Checkbox>
-            <Form.Checkbox id="checkbox-3" disabled>Option 3</Form.Checkbox>
-            <Form.Select value="default" name="select-name" onChange={() => {}}>
-              <option value="default">Select option</option>
-              <option value="1">Option 1</option>
-              <option value="2" disabled>Option 2</option>
-              <option value="3">Option 3</option>
-            </Form.Select>
-            <Form.Search submitPosition="left" />
-            <Card>
-              <h3>Card Title</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-             Ab adipisci architecto assumenda et expedita fugit id illum.</p>
-            </Card>
-            <StatusCard status="error">
-            Error
-          </StatusCard>
-            <Flyout.Wrapper>
-              <Flyout.Toggle>
-              Click me
-            </Flyout.Toggle>
-              <Flyout.Content direction="se">
-                <Card>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Ab adipisci architecto assumenda et expedita fugit id illum
-              </Card>
-              </Flyout.Content>
-            </Flyout.Wrapper>
-            <Flyout.Wrapper>
-              <Flyout.Toggle>
-                <Button node="span" size="sm" theme="alpha">
-                Github
-              </Button>
-              </Flyout.Toggle>
-              <Flyout.Content direction="ne">
-                <Card>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Ab adipisci architecto assumenda et expedita fugit id illum
-              </Card>
-              </Flyout.Content>
-            </Flyout.Wrapper>
-            <Flyout.Wrapper>
-              <Flyout.Toggle>
-              Click me
-            </Flyout.Toggle>
-              <Flyout.Content>
-                <Card>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Ab adipisci architecto assumenda et expedita fugit id illum
-              </Card>
-              </Flyout.Content>
-            </Flyout.Wrapper>
-            <Button type="submit" size="md" theme="beta">
-              Button test
-            </Button>
+
             <h3>Button Dropdowns</h3>
             <ButtonDropdown.Wrapper togglePosition="left">
               <Button node="a" to="/" size="md" theme="alpha" className="c-btn--full">
