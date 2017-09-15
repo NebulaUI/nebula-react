8087cf36149b7924f59130e4a4a14e3ee02bb741
Robert Smith <robertsmith@Roberts-MacBook-Pro.local>
2017-09-15 15:30:29 +0100

    Refactor proptypes for lists

--- src/utils/buildBreakpointValues/index.js
+++ src/utils/buildBreakpointValues/index.js
@@ -2,7 +2,8 @@ import { BREAKPOINTS } from '../../constants'
 
 const buildBreakpoints = values =>
   BREAKPOINTS.map(bp =>
-    values.map(value => `${value}@${bp}`))
+    values
+      .map(value => `${value}@${bp}`))
       .reduce((value, acc) => acc.concat(value, []))
 
 const buildBreakpointValues = values => ([
