68997425eb8708d06cd508a67e02a30af923bbbb
Robert Smith <rbrtsmith84@googlemail.com>
2017-09-16 10:47:02 +0100

    export Flyout constants

--- src/components/Flyout/index.js
+++ src/components/Flyout/index.js
@@ -1,11 +1,13 @@
 import Wrapper from './Wrapper'
 import Content from './Content'
 import Toggle from './Toggle'
+import * as constants from './constants'
 
 const Flyout = {
   Content,
   Wrapper,
-  Toggle
+  Toggle,
+  constants
 }
 
 export { Flyout }
